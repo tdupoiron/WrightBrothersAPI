bbe4723be218b809b91923643d8c74c168201a8b
Randy Pagels <pagelsr@live.com>
2024-01-27 10:58:08 -0500

    Fix bug in login functionality
    Add validation for user input
    
    Refactor code for better performance
    
    Update README with installation instructions
    
    Fix formatting issues in CSS
    
    Add error handling for database connection
    
    Update dependencies to latest versions
    
    Remove unused code
    
    Fix broken links in navigation
    
    Improve error messages for better user experience

---
+++ WrightBrothersApi/WrightBrothersApi/Models/Airport.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+
+public class Airport
+{
+    // Properties
+    public string AirportCode { get; set; }
+    public string Name { get; set; }
+    public string Location { get; set; } // This can be replaced with a more complex Location type if needed
+    public int TerminalCount { get; set; }
+    public List<string> Runways { get; set; }
+    public List<string> Airlines { get; set; }
+    public List<string> Facilities { get; set; }
+
+    // Constructor
+    public Airport(string airportCode, string name, string location, int terminalCount)
+    {
+        AirportCode = airportCode;
+        Name = name;
+        Location = location;
+        TerminalCount = terminalCount;
+        Runways = new List<string>();
+        Airlines = new List<string>();
+        Facilities = new List<string>();
+    }
+
+    // Methods
+    public void AddRunway(string runway)
+    {
+        Runways.Add(runway);
+    }
+
+    public void AddAirline(string airline)
+    {
+        Airlines.Add(airline);
+    }
+
+    public void AddFacility(string facility)
+    {
+        Facilities.Add(facility);
+    }
+
+    // This method can be expanded to integrate with real-time flight data
+    public List<Flight> GetFlights()
+    {
+        // Implementation depends on how you're managing flight data
+        throw new NotImplementedException();
+    }
+
+    // Other methods like searchAirlines, getFacilitiesInfo, getTransportOptions can be added similarly
+}

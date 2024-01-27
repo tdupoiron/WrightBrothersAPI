1d9fd494c323e65d1b2e5b4754e3f1c28b050d0c
Randy Pagels <pagelsr@live.com>
2024-01-27 11:40:20 -0500

    Delete AirportsController and Airport model, update Airfield model

--- WrightBrothersApi/WrightBrothersApi/Models/Airfield.cs
+++ WrightBrothersApi/WrightBrothersApi/Models/Airfield.cs
@@ -1,10 +1,10 @@
 public class Airfield
 {
     // Properties
-    public string Name { get; private set; }
-    public string Location { get; private set; }
-    public string DatesOfUse { get; private set; }
-    public string Significance { get; private set; }
+    public string Name { get; set; }
+    public string Location { get; set; }
+    public string DatesOfUse { get; set; }
+    public string Significance { get; set; }
 
     // Constructor
     public Airfield(string name, string location, string datesOfUse, string significance)

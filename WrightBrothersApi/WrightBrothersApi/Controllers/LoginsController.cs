42b906720e50cef2b4a3c722c7665031303886fc
Randy Pagels <pagelsr@live.com>
2024-01-28 08:07:11 -0500

    Ok

---
+++ WrightBrothersApi/WrightBrothersApi/Controllers/LoginsController.cs
@@ -0,0 +1,42 @@
+using WrightBrothersApi.Login;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WrightBrothersApi.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class LoginsController : ControllerBase
+{
+    private readonly SecureLogin _secureLogin;
+
+    public LoginsController()
+    {
+        // TODO: Replace "YourConnectionString" with your actual connection string.
+        _secureLogin = new SecureLogin("YourConnectionString");
+    }
+
+    [HttpPost]
+    public IActionResult Authenticate(string username, string password)
+    {
+        // Add fake names for testing
+        if (username == "JohnDoe" && password == "password123")
+        {
+            return Ok();
+        }
+        else if (username == "JaneDoe" && password == "password456")
+        {
+            return Ok();
+        }
+
+        bool isAuthenticated = _secureLogin.AuthenticateUser(username, password);
+
+        if (isAuthenticated)
+        {
+            return Ok();
+        }
+        else
+        {
+            return Unauthorized();
+        }
+    }
+}
